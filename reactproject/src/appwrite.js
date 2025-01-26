87cf8f5a2a45a2bd23d24da7b3dc2f3cfb8a41e7
thanmay0718 <rachatanmay0718@gmail.com>
2025-01-26 15:11:34 +0530

    Initial

--- reactproject/src/appwrite.js
+++ reactproject/src/appwrite.js
@@ -5,8 +5,9 @@ const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
-    .setEndpoint('htps://cloud.appwrite/io/v1')
+    .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(PROJECT_ID)
+
 
 const database = new Databases(client);
 
